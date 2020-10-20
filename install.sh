a3f09958e10eaa40c046c6234cb0042ed024b71a
刘博 <bo.liu@ly.com>
2020-10-20 20:59:44 +0800

    2.0.0

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 gem build staticPodGenerator.gemspec
 sudo gem uninstall staticPodGenerator -a -x
-sudo gem install staticPodGenerator-0.1.2.gem -n /usr/local/bin
-rm staticPodGenerator-0.1.2.gem
+sudo gem install staticPodGenerator-2.0.0.gem -n /usr/local/bin
+rm staticPodGenerator-2.0.0.gem
